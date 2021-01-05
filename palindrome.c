38fb5229d5d3ac48041747f2a984d6e205f0b23a
root <root@ip-172-31-46-60.ap-south-1.compute.internal>
2021-01-05 09:35:43 +0000

    Added c files

---
+++ palindrome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void palindrome() {
+    int n, reversedN = 0, remainder, originalN;
+    printf("\n Enter an integer: ");
+    scanf("%d", &n);
+    originalN = n;
+
+    // reversed integer is stored in reversedN
+    while (n != 0) {
+        remainder = n % 10;
+        reversedN = reversedN * 10 + remainder;
+        n /= 10;
+    }
+
+    // palindrome if orignalN and reversedN are equal
+    if (originalN == reversedN)
+        printf("%d is a palindrome.\n", originalN);
+    else
+        printf("%d is not a palindrome.\n", originalN);
+
+   // return 0;
+}
